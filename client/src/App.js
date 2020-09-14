7b17c2103a462d7595dd488ab3e87707c7b4f855
Suzanne Cabral <suzannecabral@live.com>
2020-09-14 14:18:56 -0700

    Added imports to app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-
+import { Route, Link, Switch } from 'react-router-dom'
 import SavedList from './Movies/SavedList';
+
+import MovieList from './Movies/MovieList'
+import Movie from './Movies/Movie'
 
 export default function App () {
   const [saved, setSaved] = useState([]); // Stretch: the ids of "saved" movies
@@ -28,12 +31,13 @@ export default function App () {
     // This is stretch. Prevent the same movie from being "saved" more than once
   };
 
-  
+
   return (
     <div>
       <SavedList list={[ /* This is stretch */]} />
-
-      <div>Replace this Div with your Routes</div>
+      <div>
+        
+      </div>
     </div>
   );
 }
